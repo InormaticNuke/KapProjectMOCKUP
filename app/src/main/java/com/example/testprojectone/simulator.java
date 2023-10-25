7dc32dab70592b3ca383244d60cacf667b08da1d
InformaticNuke <InformaticNuke@gmail.com>
2023-10-25 14:44:53 -0300

    V0.71

--- app/src/main/java/com/example/testprojectone/simulator.java
+++ app/src/main/java/com/example/testprojectone/simulator.java
@@ -1,8 +1,30 @@
 package com.example.testprojectone;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
-public class simulator {
+public class simulator extends AppCompatActivity{
+    protected void onClickDataShow(View view){
+        Intent intent = new Intent (this, selector.class);
+        startActivity(intent);
+    }
+
+    protected void onClickSelectorTwo(View view){
+        Intent intent = new Intent (this, selector.class);
+        startActivity(intent);
+    }
+
+    Integer documentValue;
+    String documentDate;
+    String documentExp;
+
+    EditText editText = (EditText) findViewById(R.id.factValue);
+    editText.setOnClickListener (new View.OnClickListener(){
+
+    }
+
+
 }
