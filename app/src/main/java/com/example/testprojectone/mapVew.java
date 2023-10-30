ef1befed003da555c30a021bc39c1704b2686031
InformaticNuke <InformaticNuke@gmail.com>
2023-10-30 02:05:52 -0300

    v01.03

---
+++ app/src/main/java/com/example/testprojectone/mapVew.java
@@ -0,0 +1,25 @@
+package com.example.testprojectone;
+
+
+public class mapVew {
+
+    private static final double EARTH_RADIUS = 6371;
+
+    /*public static double mapView(GeoPoint point1, GeoPoint point2) {
+        double lat1 = Math.toRadians(point1.getLatitude());
+        double lon1 = Math.toRadians(point1.getLongitude());
+        double lat2 = Math.toRadians(point2.getLatitude());
+        double lon2 = Math.toRadians(point2.getLongitude());
+
+        double dLat = lat2 - lat1;
+        double dLon = lon2 - lon1;
+
+        double a = Math.sin(dLat / 2) * Math.sin(dLat / 2) +
+                Math.cos(lat1) * Math.cos(lat2) * Math.sin(dLon / 2) * Math.sin(dLon / 2);
+
+        double c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+
+        return EARTH_RADIUS * c;
+    }*/
+
+}
